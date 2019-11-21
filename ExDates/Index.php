4a06a1cee7192ba3f7388df1761cbcfe1dfbd24f
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2019-11-21 12:14:12 +0100

    wip

---
+++ ExDates/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercice dates</title>
+</head>
+<body>
+<?php
+echo "1) ".date("l j M Y")."<br>";
+echo "2) ".date("M d Y")."<br>";
+/*echo "3) ".date("l j M Y")."<br>";
+echo "4) ".date("l j M Y")."<br>";
+echo "5) ".date("l j M Y")."<br>";*/
+?>
+</body>
+</html>
