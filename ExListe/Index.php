3b88dfd4dbe322e09221c8f4f1c3cb5180bc3e73
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2019-11-29 14:27:55 +0100

    fin ex liste

--- ExListe/Index.php
+++ ExListe/Index.php
@@ -6,20 +6,14 @@
     <script src="js/script.js"></script>
 </head>
 <body>
-<?php
-// TODO : Ecrire le php qui remplace le html dans la balise ul ci-dessous
-?>
-
 <h1>SI-CMI2a</h1>
 <ul>
-    <li id="div1">Fabien</li>
-    <li id="div2">David</li>
-    <li id="div3">Miguel</li>
-    <li id="div4">Benoît</li>
-    <li id="div5">Johnny</li>
-    <li id="div6">Kevin</li>
-    <li id="div7">Christopher</li>
-    <li id="div8">Dmitri</li>
+<?php
+$name = array("Fabien","David","Miguel","Benoît","Johnny","Kevin","Christopher","Dmitri");
+    for($i = 0; $i < 8;$i++){
+       echo "<li>$name[$i]</li>";
+    }
+?>
     <li id="div9">etc...</li>
 </ul>
 </body>
