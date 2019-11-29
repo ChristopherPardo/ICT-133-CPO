f411d8547a66701989d72c0e6095d632aef4ee06
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2019-11-29 14:13:35 +0100

    fin ex date et liste

--- ExDates/Index.php
+++ ExDates/Index.php
@@ -6,11 +6,13 @@
 </head>
 <body>
 <?php
-echo "1) ".date("l j M Y")."<br>";
-echo "2) ".date("M d Y")."<br>";
-/*echo "3) ".date("l j M Y")."<br>";
-echo "4) ".date("l j M Y")."<br>";
-echo "5) ".date("l j M Y")."<br>";*/
+date_default_timezone_set('europe/zurich');
+$date = strtotime('18:24:08 6 september 2019');
+echo "1) ".date("l j M Y", $date )."<br>";
+echo "2) ".date("M dS Y", $date)."<br>";
+echo "3) ".date("d/m/y g:i a", $date)."<br>";
+echo "4) ".date("d M Y, h:i:s", $date)."<br>";
+echo "5) ".date("D, d M Y h:i:s O", $date);
 ?>
 </body>
 </html>
---
+++ ExListe/Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="js/script.js"></script>
+</head>
+<body>
+<?php
+// TODO : Ecrire le php qui remplace le html dans la balise ul ci-dessous
+?>
+
+<h1>SI-CMI2a</h1>
+<ul>
+    <li id="div1">Fabien</li>
+    <li id="div2">David</li>
+    <li id="div3">Miguel</li>
+    <li id="div4">Benoît</li>
+    <li id="div5">Johnny</li>
+    <li id="div6">Kevin</li>
+    <li id="div7">Christopher</li>
+    <li id="div8">Dmitri</li>
+    <li id="div9">etc...</li>
+</ul>
+</body>
+</html>
